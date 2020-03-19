31764122a4c183c32cd536e759d98d992aa11cd3
MeriDK <ma.rizhko@gmail.com>
2020-03-19 19:26:46 +0200

    Add angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
